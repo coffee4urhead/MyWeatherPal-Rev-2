99b04515d6e05c5c34c65db4a962adce2f581fb6
Mihail <dark1982soul@gmail.com>
2025-03-29 14:34:23 +0200

    changes to the vite config file

--- MyWeatherPal/vite.config.ts
+++ MyWeatherPal/vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif'],
+  base: '/MyWeatherPal-Rev-2/', // MUST match your repo name
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        entryFileNames: 'assets/[name]-[hash].js',
+      }
+    }
+  },
+  assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.svg'],
+  publicDir: 'public'
 })
